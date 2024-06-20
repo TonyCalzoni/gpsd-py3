a5ee096ce7dee8405f5c119a78b111f2278180a2
TonyCalzoni <davis.t1887@gmail.com>
2024-06-19 21:14:45 -0400

    Update __init__.py
    Method `connect` will now only create a new connection if the GPS socket is not initialized.
    This is to help mitigate my potential shoddy handling due to a lack of a disconnect or close function within this library as installed with pip.

--- gpsd/__init__.py
+++ gpsd/__init__.py
@@ -250,23 +250,24 @@ def connect(host="127.0.0.1", port=2947):
     """
     global gpsd_socket, gpsd_stream, verbose_output, state
     logger.debug("Connecting to gpsd socket at {}:{}".format(host, port))
-    gpsd_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    gpsd_socket.connect((host, port))
-    gpsd_stream = gpsd_socket.makefile(mode="rw")
-    logger.debug("Waiting for welcome message")
-    welcome_raw = gpsd_stream.readline()
-    welcome = json.loads(welcome_raw)
-    if welcome['class'] != "VERSION":
-        raise Exception(
-            "Unexpected data received as welcome. Is the server a gpsd 3 server?")
-    logger.debug("Enabling gps")
-    gpsd_stream.write('?WATCH={"enable":true}\n')
-    gpsd_stream.flush()
+    if (gpsd_socket == None):
+        gpsd_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        gpsd_socket.connect((host, port))
+        gpsd_stream = gpsd_socket.makefile(mode="rw")
+        logger.debug("Waiting for welcome message")
+        welcome_raw = gpsd_stream.readline()
+        welcome = json.loads(welcome_raw)
+        if welcome['class'] != "VERSION":
+            raise Exception(
+                "Unexpected data received as welcome. Is the server a gpsd 3 server?")
+        logger.debug("Enabling gps")
+        gpsd_stream.write('?WATCH={"enable":true}\n')
+        gpsd_stream.flush()
 
-    for i in range(0, 2):
-        raw = gpsd_stream.readline()
-        parsed = json.loads(raw)
-        _parse_state_packet(parsed)
+        for i in range(0, 2):
+            raw = gpsd_stream.readline()
+            parsed = json.loads(raw)
+            _parse_state_packet(parsed)
 
 
 def get_current():
